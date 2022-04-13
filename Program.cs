8cedf2c0799a0d8d4b99061894c6f34881998e0f
Marcio Rela <marciorela@hotmail.com>
2022-04-13 09:53:28 -0300

    Resposta às perguntas recebidas via redis subscriber

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+using System.Data;
+
+string connectionString = "20.225.241.127";
+var redis = ConnectionMultiplexer.Connect(connectionString);
+var sub = redis.GetSubscriber();
+var db = redis.GetDatabase();
+
+sub.Subscribe("perguntas").OnMessage(m =>
+{
+    var membros = m.ToString().Split(":");
+    var pergunta = membros[2].Replace("Quanto é", "").Replace("?","");
+
+    var x = new DataTable();
+    var rsp = x.Compute(pergunta, "");
+    
+    var id = membros[1];
+
+    Console.WriteLine(m);
+    db.HashSet(id, "AMAE", "Essa pergunta tah mais facil ainda. A resposta eh " + rsp.ToString());
+});
+
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fiap-trabalho-redis-subscriber
-Trabalho da FIAP
+
+    Trabalho da FIAP
+## Curso
+MBA em Arquitetura e Desenvolvimento na Plataforma .NET 
+
+
+## Disciplina
+Arquitetura de Banco de Dados e Persistência 
+
+## Objetivo
+Fazer a inscrição em um canal, analisar as perguntas de cálculo mátemático e gerar a resposta no redis.
+
+## Integrantes:
+- Aldo
+- Anderson
+- Edenilson
+- Rela
